89e42307077b4a547e89d03d7a46b61f9e246917
RodrigoDiasDeOliveira <rodrigodigau02@gmail.com>
2026-07-08 14:14:01 +0000

    update module audit

--- shared/src/main/java/com/triminds/security/shared/web/SecurityHeaders.java
+++ shared/src/main/java/com/triminds/security/shared/web/SecurityHeaders.java
@@ -1,7 +1,12 @@
 package com.triminds.security.shared.web;
 
 public final class SecurityHeaders {
+
     public static final String TENANT_ID = "X-Tenant-Id";
+
+    public static final String IDENTITY_ID = "X-Identity-Id";
+
+    public static final String REQUEST_ID = "X-Request-Id";
 
     private SecurityHeaders() {
     }
